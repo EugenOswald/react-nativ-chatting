ac7b825f80f7839c09896c9ae88b88a4d7b8f17f
Eugen Oswald <eugen.oswald@kemweb.de>
2023-11-14 16:49:12 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import bg from './assets/bg.png';
+import ChatItem from './ChatItem';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<ImageBackground source={bg} style={styles.container}>
+			<StatusBar style='auto' />
+			<ChatItem></ChatItem>
+			<ChatItem></ChatItem>
+			<ChatItem></ChatItem>
+			<ChatItem></ChatItem>
+		</ImageBackground>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#484961',
+		alignItems: 'center',
+		justifyContent: 'start',
+		paddingTop: 70,
+	},
 });
---
+++ ChatItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const ChatItem = () => {
+	return <Text>User Name</Text>;
+};
+
+export default ChatItem;
